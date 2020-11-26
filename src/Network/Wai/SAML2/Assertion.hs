0c945e5af4391a3e1238735b3e3f45f1842308bc
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2020-11-26 18:09:23 +0900

    add subjectNameId to Subject

--- src/Network/Wai/SAML2/Assertion.hs
+++ src/Network/Wai/SAML2/Assertion.hs
@@ -82,16 +82,17 @@ instance FromXML SubjectConfirmation where
 -- | The subject of the assertion.
 data Subject = Subject {
     -- | The list of subject confirmation elements, if any.
-    subjectConfirmations :: ![SubjectConfirmation]
+    subjectConfirmations :: ![SubjectConfirmation],
+    subjectNameId :: !T.Text
 } deriving (Eq, Show)
 
 instance FromXML Subject where 
     parseXML cursor = do 
         confirmations <- sequence $ 
             cursor $/ element (saml2Name "SubjectConfirmation") &| parseXML
-
         pure Subject{
-            subjectConfirmations = confirmations
+            subjectConfirmations = confirmations,
+            subjectNameId        = T.concat $ cursor $/ element (saml2Name "NameID") &/ content
         }
 
 --------------------------------------------------------------------------------
