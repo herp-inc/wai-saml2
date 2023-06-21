46d7c60da5e63c78a33d8445502d0ed802657b2b
hiroqn <909385+hiroqn@users.noreply.github.com>
2023-06-21 08:14:21 +0000

    test

--- tests/validate.hs
+++ tests/validate.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE LambdaCase #-}
 import Crypto.PubKey.RSA (PublicKey)
+import Crypto.Hash
+import Control.Monad.Except
+
 import Network.Wai.SAML2
 import Network.Wai.SAML2.C14N
 import Network.Wai.SAML2.Validation
+import Network.Wai.SAML2.Response
+import Text.XML
 import RIO
 import System.Environment
 import qualified Data.ByteString.Base64 as BS
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.X509 as X509
 import qualified Data.X509.Memory as X509
+import qualified Text.XML as XML
+import qualified Text.XML.Cursor as XML
 
 parseCertificate :: ByteString -> PublicKey
 parseCertificate certificate = case X509.readSignedObjectFromMemory certificate of
@@ -24,10 +32,25 @@ main = getArgs >>= \case
         let pub = parseCertificate cert
             cfg = saml2ConfigNoEncryption pub
         bs <- BS.init <$> readFileBinary path
+        normalised <- runExceptT $ do 
+            (responseXmlDoc, _samlResponse) <- decodeResponse bs 
+            let docMinusSignature = removeSignature responseXmlDoc
 
-        -- c14nを適用したXMLを出力
-        canonicalise (either error id $ BS.decode bs) >>= BS.putStrLn
+            -- then render the resulting document and canonicalise it
+            let renderedXml = XML.renderLBS def docMinusSignature
+            refCanonResult <- liftIO $ try $ canonicalise (LBS.toStrict renderedXml)
 
+            normalised <- case refCanonResult of
+                Left err -> throwError $ CanonicalisationFailure err
+                Right result -> pure result
+
+            pure normalised
+        case normalised of
+            Left err -> print $ show err
+            Right n -> BS.putStrLn n
         result <- validateResponse cfg bs
         print result
+    [path] -> do
+        normalised <- readFileBinary path
+        print $ hashWith SHA256 normalised
     _ -> fail "cabal run validate okta.crt okta.b64"
